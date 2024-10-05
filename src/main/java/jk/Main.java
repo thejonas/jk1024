a2771079ce9be11b80801d4c72cdca8949de2df5
Jonas Karlsson <thejonas@gmail.com>
2024-10-05 11:05:05 -0400

    Create application

--- src/main/java/jk/Main.java
+++ src/main/java/jk/Main.java
@@ -1,7 +1,18 @@
 package jk;
 
+import java.time.LocalDate;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    final var store = new Store();
+
+    final var rentalAgreement = store.checkout(
+            args[0],
+            LocalDate.parse(args[1]),
+            Integer.parseInt(args[2]),
+            Integer.parseInt(args[3])
+    );
+
+    System.out.println(rentalAgreement);
   }
 }
